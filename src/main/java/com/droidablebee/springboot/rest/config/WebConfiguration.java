8e9ff4d661438d8a957fcad9a868b5310a64bbab
Pavel Fomin <p.v.fomin@gmail.com>
2016-08-12 22:39:57 -0500

    Removed all other dependencies and tests leaving actuator info test only.

--- src/main/java/com/droidablebee/springboot/rest/config/WebConfiguration.java
+++ src/main/java/com/droidablebee/springboot/rest/config/WebConfiguration.java
@@ -1,24 +1,11 @@
 package com.droidablebee.springboot.rest.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
-    @Override 
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-    	configurer.defaultContentType(MediaType.APPLICATION_JSON);
-    }
 }
--- src/main/java/com/droidablebee/springboot/rest/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.droidablebee.springboot.rest.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.droidablebee.springboot.rest.domain.Person;
-
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
-
-}
