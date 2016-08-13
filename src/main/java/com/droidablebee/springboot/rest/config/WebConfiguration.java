dafb2a427e8f6c9e061cb97ca552e09661cc6abc
Pavel Fomin <p.v.fomin@gmail.com>
2016-08-13 09:06:42 -0500

    Removed @EnableWebMvc which "takes complete control of Spring MVC". Taking complete control means Boot no longer configures Spring MVC's Jackson-based HTTP message converter. https://github.com/spring-projects/spring-boot/issues/6642

--- src/main/java/com/droidablebee/springboot/rest/config/WebConfiguration.java
+++ src/main/java/com/droidablebee/springboot/rest/config/WebConfiguration.java
@@ -3,12 +3,10 @@ package com.droidablebee.springboot.rest.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override 
