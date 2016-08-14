3a57639d42bccc55a241a7952bcc6555e96d0f20
Pavel Fomin <p.v.fomin@gmail.com>
2016-08-13 21:46:18 -0500

    Removed unneeded swagger resource mapping provided by boot by default.

--- src/main/java/com/droidablebee/springboot/rest/config/WebConfiguration.java
+++ src/main/java/com/droidablebee/springboot/rest/config/WebConfiguration.java
@@ -3,17 +3,10 @@ package com.droidablebee.springboot.rest.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurerAdapter {
-
-    @Override 
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
