98cfeb7e914df67ddebd10d9d097be0aa98cfea6
Pavel Fomin <p.v.fomin@gmail.com>
2022-01-25 10:53:23 -0600

    Replace deprecated WebMvcConfigurerAdapter with WebMvcConfigurer

--- src/main/java/com/droidablebee/springboot/rest/config/WebConfiguration.java
+++ src/main/java/com/droidablebee/springboot/rest/config/WebConfiguration.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
