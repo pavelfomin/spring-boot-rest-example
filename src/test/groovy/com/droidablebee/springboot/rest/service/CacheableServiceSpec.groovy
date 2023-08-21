c71e67669efc02d5ea16c5f58b1ed580c8f223f6
Pavel Fomin <p.v.fomin@gmail.com>
2023-08-21 14:55:34 -0500

    Enable caching using `@EnableCaching`

---
+++ src/test/groovy/com/droidablebee/springboot/rest/service/CacheableServiceSpec.groovy
@@ -0,0 +1,62 @@
+package com.droidablebee.springboot.rest.service
+
+import com.droidablebee.springboot.rest.domain.Person
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import spock.lang.Specification
+
+@SpringBootTest
+class CacheableServiceSpec extends Specification {
+
+    @Autowired
+    CacheableService cacheableService
+
+    @SpringBean
+    CacheableService.CacheableServiceClient cacheableServiceClient = Mock()
+
+    void setup() {
+
+        0 * _
+    }
+
+    def "retrieve - cached"() {
+
+        given:
+        PageRequest pageRequest1 = PageRequest.of(0, 10)
+        PageRequest pageRequest2 = PageRequest.of(0, 20)
+        Page<CacheableService.Result> page1 = Mock()
+        Page<CacheableService.Result> page2 = Mock()
+
+        when: "first service call is made"
+        Page<CacheableService.Result> result = cacheableService.retrieve(pageRequest1)
+
+        then: "client method is called"
+        1 * cacheableServiceClient.retrieve(pageRequest1) >> page1
+
+        result == page1
+
+        when: "subsequent service call is made"
+        Page<CacheableService.Result> cached = cacheableService.retrieve(pageRequest1)
+
+        then: "cached value is returned w/out client method call"
+        cached == result
+
+        when: "different parameter is used"
+        result = cacheableService.retrieve(pageRequest2)
+
+        then: "client method is called"
+        1 * cacheableServiceClient.retrieve(pageRequest2) >> page2
+
+        result == page2
+
+        when: "subsequent service call is made"
+        cached = cacheableService.retrieve(pageRequest2)
+
+        then: "cached value is returned w/out client method call"
+        cached == result
+    }
+
+}
