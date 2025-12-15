9893ae8c8aa151a684ac75bc428aec2bccc2ca39
Pavel.Fomin <pvfomin@gmail.com>
2025-12-15 09:22:13 -0600

    id 'org.springframework.boot' version '3.5.7'

--- src/test/groovy/com/droidablebee/springboot/rest/endpoint/BaseEndpointSpec.groovy
+++ src/test/groovy/com/droidablebee/springboot/rest/endpoint/BaseEndpointSpec.groovy
@@ -40,6 +40,7 @@ abstract class BaseEndpointSpec extends Specification {
 
         jwtDecoder.decode("invalid") >> { String token -> throw new BadJwtException("Mocked token '$token' is invalid") }
         jwtDecoder.decode(_ as String) >> jwt
+        jwt.getClaims() >> [:] // required by org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter#isDPoPBoundAccessToken
     }
 
     /**
